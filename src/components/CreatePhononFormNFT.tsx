8a8a82f5ac3238eaeceffa9d282df4af39cf9b4e
Anthony Hinchliff <anthony.hinchliff@gmail.com>
2022-03-22 08:27:58 +1000

    use correct tag name

--- src/components/CreatePhononFormNFT.tsx
+++ src/components/CreatePhononFormNFT.tsx
@@ -28,7 +28,7 @@ export const CreateNFTPhononForm: React.FC<{
             TagName: "TagPhononContractAddress",
             TagValue: data.contractAddress,
           },
-          { TagName: "TagPhononTokenId", TagValue: data.tokenId },
+          { TagName: "TagPhononContractTokenId", TagValue: data.tokenId },
         ],
       },
     ]);
